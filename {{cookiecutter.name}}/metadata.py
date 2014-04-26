3ffc1d158d20eae7a580995aaeb3cce41904c3df
Skabed <gmadorell@gmail.com>
2014-04-26 20:08:40 +0200

    Added support for different letter sizes.

--- {{cookiecutter.name}}/metadata.py
+++ {{cookiecutter.name}}/metadata.py
@@ -2,7 +2,7 @@
 
 # Global settings.
 document_class = "article"
-font_type = None
+font_size = "10pt"  # Allowed: 8pt, 9pt, 10pt, 11pt, 12pt, 14pt, 17pt, 20pt
 
 # Front page.
 ## Setting title to None also ignores authors and date.
