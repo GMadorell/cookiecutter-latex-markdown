55cbac99ede37d40d7de00f7dd3a25944c0628a4
Skabed <gmadorell@gmail.com>
2014-04-26 20:36:27 +0200

    Added make option publish that produces a pdf file. Also, make clean no longer deletes the pdf file. Changed metadata to now use cookiecutter.name as the default title.

--- {{cookiecutter.name}}/metadata.py
+++ {{cookiecutter.name}}/metadata.py
@@ -6,7 +6,7 @@ font_size = "10pt"  # Allowed: 8pt, 9pt, 10pt, 11pt, 12pt, 14pt, 17pt, 20pt
 
 # Front page.
 ## Setting title to None also ignores authors and date.
-title = "Some title"
+title = {{cookiecutter.name}}
 authors = ["Authòr1", "Author2"]
 date = r"\today"
 
